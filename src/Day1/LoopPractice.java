8cf9a3ded0f3bdbf634a5df155c0b4fb1752482b
tmcoskun <turhanmc@gmail.com>
2020-07-27 00:30:35 -0700

    Update

--- src/Day1/LoopPractice.java
+++ src/Day1/LoopPractice.java
@@ -6,6 +6,7 @@ public class LoopPractice {
         //some random code
         for (int i=0; i<10; i+=2) {
             System.out.println("Counter :: "+i);
+            System.out.println("^^^^^^");
         }
     }
 
