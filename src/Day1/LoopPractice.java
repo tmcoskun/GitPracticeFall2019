60687d42ca32dd9b1bee5a5323012607de395150
tmcoskun <turhanmc@gmail.com>
2020-07-26 23:45:10 -0700

    Added loop practice file

---
+++ src/Day1/LoopPractice.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class LoopPractice {
+
+    public static void main(String[] ars){
+        //some random code
+        for (int i=0; i<10; i+=2) {
+            System.out.println("Counter :: "+i);
+        }
+    }
+
+}
