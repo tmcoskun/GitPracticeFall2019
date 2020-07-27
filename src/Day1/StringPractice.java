df4cdfbf4339471b1685e4d816baee269fc06ab1
tmcoskun <turhanmc@gmail.com>
2020-07-26 23:15:06 -0700

    modified StringPractice class

--- src/Day1/StringPractice.java
+++ src/Day1/StringPractice.java
@@ -5,6 +5,8 @@ public class StringPractice {
 
     public static void main(String[] args){
         System.out.println("Hello World!");
+        String str = "Java is Fun!";
+        System.out.println(str);
     }
 
 
